2a7e48ce36133c42bfd55f74a6298bca01f46833
AungChan17 <chanmyae1792003@gmail.com>
2022-09-24 12:23:57 +0630

    feat[category]:Add  category blade files

--- routes/category.php
+++ routes/category.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/categories', [CategoryController::class, 'index'])
     ->name('category.index');
+
+Route::post('/categories', [CategoryController::class, 'store'])
+    ->name('category.store');
+
+Route::get('/categories/create', [CategoryController::class, 'create'])
+    ->name('category.create');
+
